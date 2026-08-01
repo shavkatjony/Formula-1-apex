385d33f1e802580ddb7f8a9546f04a9c802cc490
Shavkatjon Yuldashev <shavkatjon.yuldashev.0411@gmail.com>
2026-08-02 03:07:28 +0900

    Update driver dataset

---
+++ drivers.py
@@ -0,0 +1,174 @@
+# the dictinaries for driver informations 
+drivers = [
+     { #1
+        "name"  : "Max Verstappen",
+        "nation" : "Netherlands",
+        "team"   : "Red Bull",
+        "age"    : 28,
+        "f1_debut" : 2015,
+        "previous_teams" : ["Toro Rosso"],
+        "pole_p" : 44,
+        "wins"   : 70,
+        "championships" : 4,
+        "racing_style"  : ["agressive and late braker"],
+        "status"        : "Active"
+    },
+
+    { #2
+        "name": "Lewis Hamilton",
+        "nation": "United Kingdom",
+        "team": "Ferrari",
+        "age": 41,
+        "f1_debut": 2007,
+        "previous_teams": ["McLaren", "Mercedes"],
+        "pole_positions": 104,
+        "wins": 105,
+        "championships": 7,
+        "racing_style": ["Smooth", "Strategic"],
+        "status": "Active"
+    },
+
+    { #3
+        "name": "Charles Leclerc",
+        "nation": "Monaco",
+        "team": "Ferrari",
+        "age": 28,
+        "f1_debut": 2018,
+        "previous_teams": ["Sauber"],
+        "pole_positions": 27,
+        "wins": 8,
+        "championships": 0,
+        "racing_style": ["Aggressive", "Excellent Qualifier"],
+        "status": "Active"
+    },
+
+    { #4
+        "name": "George Russell",
+        "nation": "United Kingdom",
+        "team": "Mercedes",
+        "age": 28,
+        "f1_debut": 2019,
+        "previous_teams": ["Williams"],
+        "pole_positions": 6,
+        "wins": 4,
+        "championships": 0,
+        "racing_style": ["Consistent", "Analytical"],
+        "status": "Active"
+    },
+
+    { #5
+        "name": "Daniel Ricciardo",
+        "nation": "Australia",
+        "team": None,
+        "age": 37,
+        "f1_debut": 2011,
+        "previous_teams": [
+            "HRT",
+            "Toro Rosso",
+            "Red Bull Racing",
+            "Renault",
+            "McLaren",
+            "RB"
+        ],
+        "pole_positions": 3,
+        "wins": 8,
+        "championships": 0,
+        "racing_style": ["Late Braker", "Aggressive Overtaker"],
+        "status": "Inactive"
+    },
+    
+    { #6
+        "name": "Valtteri Bottas",
+        "nation": "Finland",
+        "team": "Cadillac",
+        "age": 37,
+        "f1_debut": 2013,
+        "previous_teams": [
+            "Williams",
+            "Mercedes",
+            "Alfa Romeo",
+            "Sauber"
+        ],
+        "pole_positions": 20,
+        "wins": 10,
+        "championships": 0,
+        "racing_style": ["Smooth", "Consistent"],
+        "status": "Active"
+    },
+
+    { #7
+        "name": "Sergio Perez",
+        "nation": "Mexico",
+        "team": "Cadillac",
+        "age": 36,
+        "f1_debut": 2011,
+        "previous_teams": [
+            "Sauber",
+            "McLaren",
+            "Force India",
+            "Racing Point",
+            "Red Bull Racing"
+        ],
+        "pole_positions": 3,
+        "wins": 6,
+        "championships": 0,
+        "racing_style": ["Tire Whisperer", "Patient"],
+        "status": "Active"
+    },
+
+    { #8
+        "name": "Andrea Kimi Antonelli",
+        "nation": "Italy",
+        "team": "Mercedes",
+        "age": 20,
+        "f1_debut": 2025,
+        "previous_teams": [],
+        "pole_positions": 1,
+        "wins": 1,
+        "championships": 0,
+        "racing_style": ["Fearless", "Fast Learner"],
+        "status": "Active"
+    },
+
+    { #9
+        "name": "Lando Norris",
+        "nation": "United Kingdom",
+        "team": "McLaren",
+        "age": 27,
+        "f1_debut": 2019,
+        "previous_teams": [],
+        "pole_positions": 12,
+        "wins": 9,
+        "championships": 1,
+        "racing_style": ["Smooth", "Consistent"],
+        "status": "Active"
+    },
+
+    { #10
+        "name": "Oscar Piastri",
+        "nation": "Australia",
+        "team": "McLaren",
+        "age": 25,
+        "f1_debut": 2023,
+        "previous_teams": [],
+        "pole_positions": 10,
+        "wins": 8,
+        "championships": 1,
+        "racing_style": ["Calm", "Highly Efficient"],
+        "status": "Active"
+    },
+
+    { #11
+        "name": "Gabriel Bortoleto",
+        "nation": "Brazil",
+        "team": "Audi",
+        "age": 22,
+        "f1_debut": 2025,
+        "previous_teams": [],
+        "pole_positions": 0,
+        "wins": 0,
+        "championships": 0,
+        "racing_style": ["Calculated", "Technical"],
+        "status": "Active"
+    },
+]
